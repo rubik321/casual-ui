8b2c9fdd8fd6c8fedab61223928c03109d2cda04
Nguyen Xuan Manh <NguyenXuanManhLnbg@gmail.com>
2024-01-23 16:11:23 +0700

    update

---
+++ Assets/Scripts/Collected/CollectedItem.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Collected : MonoBehaviour
+{
+    public Image boxImg, IconCollectedImg;
+    public TextMeshProUGUI nameTxt, levelTxt;
+    public Collected data;
+    
+
+}
